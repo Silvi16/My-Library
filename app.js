21f5ffd17f82a03b48a6f0ff924a6d212d8e152f
Silvia Krajnakova <silvia.krajnakova@gmail.com>
2021-06-13 14:05:35 +0200

    Add request and create page

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.get('/', (req, res) => {
     res.render('index.ejs', {root: __dirname})
 })
 
+app.get('/create', (req, res) => {
+    res.render('create.ejs', {root: __dirname})
+})
+
+app.get('/request', (req, res) => {
+    res.render('request.ejs', {root: __dirname})
+})
+
+app.get('/sign_in', (req, res) => {
+    res.render('sign_in.ejs', {root: __dirname})
+})
+
+app.get('/quotes', (req, res) => {
+    res.render('quotes.ejs', {root: __dirname})
+})
+
 app.listen(8000, () => {
     console.log('Listening on port 8000')
 })
